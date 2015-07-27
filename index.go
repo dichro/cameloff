c0489f6e94b2c5904300c0bbb7d8206edb7a4031
Miki Habryn <dichro@rcpt.to>
2015-07-26 19:48:00 -0700

    count fetches

--- index.go
+++ index.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"sync"
 
 	"camlistore.org/pkg/blob"
+	"camlistore.org/pkg/blobserver"
 	"camlistore.org/pkg/blobserver/dir"
 	"camlistore.org/pkg/index"
 )
@@ -13,6 +16,20 @@ import (
 var (
 	blobDir = flag.String("blob_dir", "", "Camlistore blob directory")
 )
+
+type fetcher struct {
+	blobserver.FetcherEnumerator
+
+	mu      sync.Mutex
+	fetches int
+}
+
+func (f *fetcher) Fetch(ref blob.Ref) (io.ReadCloser, uint32, error) {
+	f.mu.Lock()
+	f.fetches++
+	f.mu.Unlock()
+	return f.FetcherEnumerator.Fetch(ref)
+}
 
 func main() {
 	flag.Parse()
@@ -26,11 +43,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	f := &fetcher{FetcherEnumerator: s}
 	in := index.NewMemoryIndex()
-	in.InitBlobSource(s)
+	in.InitBlobSource(f)
 
 	for i, arg := range flag.Args() {
-		fmt.Println(i, "index entries", count(in))
+		fmt.Println(i, "index entries", count(in), "fetches", f.fetches)
 		br, ok := blob.Parse(arg)
 		if !ok {
 			log.Fatal("unparseable ", arg)
@@ -46,7 +64,7 @@ func main() {
 		}
 		r.Close()
 	}
-	fmt.Println("end index entries", count(in))
+	fmt.Println("end index entries", count(in), "fetches", f.fetches)
 }
 
 func count(in *index.Index) (entries int) {
